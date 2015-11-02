e39e7f255c8e810a066baa53934d4269375f274a
James Anelay <jamesanelay@gmail.com>
2015-11-02 22:40:05 +0000

    Change override to do only the main thing we want (remove <p></p> wrapping blocks)

--- js/theextensionlab/tinymceconfig/adminhtml/wysiwyg/tiny_mce/setup-override.js
+++ js/theextensionlab/tinymceconfig/adminhtml/wysiwyg/tiny_mce/setup-override.js
@@ -6,12 +6,7 @@ if(window.tinyMceWysiwygSetup)
 
         var settings = this.originalGetSettings(mode);
 
-        settings.extended_valid_elements = '+div[*],+a[*],+span[*]';
-        settings.valid_children = 'a[h1|h2|h3|h4|h5|h6|p|span|div|img]';
-        settings.force_br_newlines = true;
-        settings.force_p_newlines = false;
         settings.forced_root_block = false;
         return settings;
-
     }
 }
